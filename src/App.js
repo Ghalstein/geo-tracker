dc8481281ccde12ddf803ecea00e96d297a3cc30
Garrett Halstein <garrett.halstein@emory.edu>
2019-10-07 21:15:42 -0400

    updated geo-location to class based component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
   window.navigator.geolocation.getCurrentPosition(
     (position) => console.log(position),
     (err) => console.log(err)
   );
 
-  return ( <
-    div className = "App" >
-    </div>
-  );
+  render() {
+    return ( 
+      <div className = "App" >
+        Lattitude: 
+      </div>
+    );
+  };
 }
 
 export default App;
