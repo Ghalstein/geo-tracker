97789d3d38ca29b4a37c9e7ef2918f1de6c621eb
Garrett Halstein <garrett.halstein@emory.edu>
2019-10-07 22:59:39 -0400

    season tracker working properly

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,7 +1,43 @@
 import React from "react";
 
+const getSeason = (lat, month) => {
+  if (month > 11 || month < 3) {
+    if (lat > 0) {
+      return "Winter.";
+    } else {
+      return "Summer";
+    }
+  } else if (month < 12 && month > 8) {
+    if (lat > 0) {
+      return "Fall";
+    } else {
+      return "Spring";
+    }
+  } else if (month < 9 && month > 5) {
+    if (lat > 0) {
+      return "Summer";
+    } else {
+      return "Winter.";
+    }
+  } else {
+    if (lat > 0) {
+      return "Spring";
+    } else {
+      return "Fall";
+    }
+  }
+};
+
 const SeasonDisplay = props => {
-  return <h1>{props.latitude}</h1>;
+  return (
+    <div>
+      <h1>
+        The season is currently{" "}
+        {getSeason(props.latitude, new Date().getMonth())}
+        {""} where you reside.
+      </h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
